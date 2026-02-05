f02d3954ae5ec93d3a41aa44245b55e1f216341e
gabriel-anzolin <gabriel_anzolin@hotmail.com>
2026-02-05 15:45:00 -0300

    Teste

--- scripts/Primeiro_Script.py
+++ scripts/Primeiro_Script.py
@@ -7,3 +7,35 @@ Primeiro script no Python
 
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Função para gerar um plot de séries temporais
+def timeSeriesPlot(arr):
+    """
+    
+
+    Parameters
+    ----------
+    arr : np.array
+        array numpy com a matriz de dados.
+
+    Returns
+    -------
+    fig : TYPE
+        DESCRIPTION
+        
+    Exemplo de uso
+    fig = timeSeriesPlot(arr)
+    
+    """
+    
+    arr_average = np.mean(arr, axis = 1)
+    fig, ax = plt.subplots(2)
+    ax[0].plot(arr_average)
+    ax[1].plot(arr)
+       
+    fig.savefig("C:\\Users\\gabri\\Documents\\GitHub\\minicurso_LABHIDRO\\figuras\\timeSeriesPlot.png", dpi = 300)
+    
+    return fig
+    
---
+++ scripts/run_PrimeiroScript.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 15:30:00 2026
+
+@author: gabri
+"""
+
+import Primeiro_Script
+
+import numpy as np
+
+arr = np.random.rand(100,100)
+
+Primeiro_Script.timeSeriesPlot(arr)
